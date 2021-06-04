bae4d0ee586077d7185a0b9791abaf9335df21a3
yerim926 <yerim926@users.noreply.github.com>
2021-06-04 12:32:11 -0400

    revising contents

--- routes/index.js
+++ routes/index.js
@@ -3,32 +3,32 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home'});
+  res.render('index', { title: 'Home', page: 'home'});
 });
 
 /* GET home page. */
-router.get('/home', function(req, res, next) {
-  res.render('home', { title: 'Home'});
+router.get('home', function(req, res, next) {
+  res.render('home', { title: 'Home', page: 'home'});
 });
 
 /* GET About page. */
 router.get('/about', function(req, res, next) {
-  res.render('aboutMe', { title: 'About'});
+  res.render('aboutMe', { title: 'About', page: 'about'});
 });
 
 /* GET Products page. */
 router.get('/projects', function(req, res, next) {
-  res.render('projects', { title: 'Projects'});
+  res.render('projects', { title: 'Projects', page: 'projects'});
 });
 
 /* GET Services page. */
 router.get('/services', function(req, res, next) {
-  res.render('services', { title: 'Services'});
+  res.render('services', { title: 'Services', page:'services'});
 });
 
 /* GET Contact Us page. */
 router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'Contact'});
+  res.render('contact', { title: 'Contact', page: 'contact'});
 });
 
 module.exports = router;
